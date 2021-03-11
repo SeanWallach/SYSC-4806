edf6b68b562ae789d4d63c7a2f69d0585f9f392d
everettsoldaat <everett.soldaat@gmail.com>
2021-03-11 15:40:45 -0500

    Created Cart entity and CartRepo

---
+++ src/main/java/Entities/Cart.java
@@ -0,0 +1,31 @@
+package Entities;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToMany
+    private List<Book> books;
+
+
+    public Cart(){ books = new ArrayList<Book>(); }
+
+    public long getId() { return id;}
+
+    public void setId(long id) {this.id = id;}
+
+    public void removeBook(Book bookToRemove) {books.remove(bookToRemove);}
+
+    public void addBook(Book bookToAdd) {books.add(bookToAdd);}
+
+}
