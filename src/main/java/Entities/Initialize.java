b705750617ebe246a0da27b1e19a929b34354204
Ajfoster4444 <ajfoster@magma.ca>
2021-03-17 13:47:59 -0400

    Created a page to create new users. Also added password detection on user and owner login. The login page will now differentiate between a user and owner and show them their relevant homepage when they log in.

--- src/main/java/Entities/Initialize.java
+++ src/main/java/Entities/Initialize.java
@@ -1,7 +1,6 @@
 package Entities;
 
 import Entities.Repositories.*;
-import javafx.scene.canvas.GraphicsContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
