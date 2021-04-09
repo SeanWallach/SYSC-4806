ff4547f26141cdc2c79b5c446c5c279c859b8276
LAPTOP-ASUS\Sean <wallachsean1@gmail.com>
2021-04-09 01:21:03 -0400

    removed compiler.xml changes

--- src/main/java/Entities/Repositories/BookRepo.java
+++ src/main/java/Entities/Repositories/BookRepo.java
@@ -1,10 +1,8 @@
 package Entities.Repositories;
 
 import Entities.*;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface BookRepo extends CrudRepository<Book, Long>{
     Book findById(long ISBN);
